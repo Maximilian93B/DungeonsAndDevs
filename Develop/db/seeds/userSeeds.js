f5abc30cde85996c0b0c526982a4214bc1d66d38
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-02-14 14:41:15 -0400

    added userSeeds.js

---
+++ Develop/db/seeds/userSeeds.js
@@ -0,0 +1,25 @@
+// Used to Seed User Data 
+
+const { User } = require('../../Models/User');
+
+async function seedUsers(){
+
+    try {
+        await User.create({
+            username: 'Joe Daily',
+            email:'joedaily@example.com',
+            password: 'password123',
+            points: 100,sign_up_date: new Date(),
+            last_login: new Date(),
+            profile: 'Profile description for John Doe.'
+        });
+
+
+        console.log('Users seeded successfully');
+    } catch (error) {
+      console.error('Error seeding users:', error);
+    }
+  }
+    
+
+seedUsers();
