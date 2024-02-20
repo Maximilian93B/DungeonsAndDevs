07cacec4cf8eea8e5eeaa061d942fa37eae1942e
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-02-20 18:27:03 -0400

    added challenge 1 validation

--- Develop/public/js/mainPage.js
+++ Develop/public/js/mainPage.js
@@ -222,7 +222,17 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+// CHALLENGE 1 verification 
 
+function containsGitHub(url) {
+    return url.includes("github.com");
+}
+
+function verifyGitHub(){
+    const url = document.getElementById('urlInput').value;
+    const result = containsGitHub(url)
+    document.getElementById('result').textContent = result ? "Valid GitHub URL" : "Invalid Github URl";
+}
 
 
 
